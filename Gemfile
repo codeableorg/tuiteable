7384733652db3de2dcc07b0bd21fffe86c04f837
Albert <acastemoreno@gmail.com>
2020-06-03 17:25:14 -0500

    seed

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'omniauth-facebook'
 gem 'omniauth-github'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'bcrypt'
+require 'faker'
+
+p "Seed admins"
+
+admins = [
+  {
+    "username" => "acastemoreno",
+    "name" => "Albert",
+    "email" => "acastemoreno@gmail.com",
+    "password" => "123456",
+    "encrypted_password" => BCrypt::Password.create("123456")
+  },
+  {
+    "username" => "MariellaUgarte",
+    "name" => "Mariella",
+    "email" => "juanjoseuagrtellamocca@gmail.com",
+    "password" => "123456",
+    "encrypted_password" => BCrypt::Password.create("123456")
+  },
+  {
+    "username" => "Saidab1",
+    "name" => "Saida",
+    "email" => "saidabrito57@gmail.com",
+    "password" => "123456",
+    "encrypted_password" => BCrypt::Password.create("123456")
+  }
+]
+
+admins.map{|admin| User.create(admin)}
+
+p "Seed regular user"
+
+users = []
+20.times do |i|
+  password = Faker::Internet.password
+  users << { name: Faker::Name.unique.name, username: Faker::Internet.unique.username, email: Faker::Internet.email, encrypted_password: BCrypt::Password.create(password), created_at: Faker::Time.between(from: 3.days.ago, to: Time.now), updated_at: Time.now }
+end
+User.insert_all!(users)
+
+p "Seed tweets"
+tweets = []
+30.times do |i|
+  tweets << { owner_id: rand(1..23), body: Faker::Lorem.sentence, created_at: Faker::Time.between(from: 3.days.ago, to: Time.now), updated_at: Time.now }
+end
+Tweet.insert_all!(tweets)
+
+p "Seed comments"
+comments = []
+30.times do |i|
+  comments << { user_id: rand(1..23), tweet_id: rand(1..30), body: Faker::Lorem.sentence, created_at: Faker::Time.between(from: 3.days.ago, to: Time.now), updated_at: Time.now }
+end
+Comment.insert_all!(comments)
+
+p "Seed likes"
+
+tweets = Tweet.all
+
+User.all.map do |user|
+  user.liked_tweets << tweets.sample(rand(1..30))
+end
