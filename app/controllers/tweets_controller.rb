f5a9e41465e146e47c00106746a25cd8146a05f4
Marvin Carrasco <marccenvivo@gmail.com>
2020-06-04 18:25:59 -0500

    add layout

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,9 @@
 class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all.order(created_at: :desc)
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'tweets#index'
 
+  resources :tweets, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
