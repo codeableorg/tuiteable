901494a2426861250fcb45646e54cc26476f9882
Josue Narro <josue.narro.a@gmail.com>
2020-06-04 15:03:22 -0500

    Tuit's show view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   resources :tuits
-  root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root to: 'home#index'
+  root to: 'tuits#index'
 end
