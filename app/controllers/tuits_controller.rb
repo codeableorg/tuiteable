680d22b09d2e015d6ad4540164a10e12a52fb362
FrankFlores <fraco.1918@gmail.com>
2020-06-03 22:41:51 -0500

    controllers

--- app/controllers/tuits_controller.rb
+++ app/controllers/tuits_controller.rb
@@ -3,15 +3,25 @@ class TuitsController < ApplicationController
   end
 
   def show
+    @tuit = Tuit.find(params[:id])
+    # @vote = Vote.find_by(user: current_user, meme: @meme)
   end
 
   def new
+    @tuit = Tuit.new
   end
 
   def edit
   end
 
   def create
+    @tuit = Tuit.new(tuit_params)
+    @tuit.owner = current_user
+    if @tuit.save
+      redirect_to tuit_path(@tuit)
+    else
+      render :new
+    end
   end
 
   def update
