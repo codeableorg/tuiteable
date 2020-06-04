11ced712cd8924a2a19789e1027569f318a8e14c
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-06-04 16:13:43 -0500

    user can see and react to specific tuit

--- app/controllers/tuits/likes_controller.rb
+++ app/controllers/tuits/likes_controller.rb
@@ -2,5 +2,14 @@ class Tuits::LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    tuit = Tuit.find(params[:tuit_id])
+    like = Like.find_by(user: current_user, tuit: tuit)
+    if like
+      redirect_to tuit_path(tuit), alert: 'You already have liked this tuit.'
+    elsif tuit.likes.new(user: current_user)&.save
+      redirect_to tuit_path(tuit)
+    else
+      redirect_to tuit_path(tuit), alert: 'We could not add your like.'
+    end
   end
 end
