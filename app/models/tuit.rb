63295548327cf2206cc982f19791978a03981a55
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-06-03 20:36:50 -0500

    added model associations

--- app/models/tuit.rb
+++ app/models/tuit.rb
@@ -1,3 +1,6 @@
 class Tuit < ApplicationRecord
-  belongs_to :owner_id
+  belongs_to :owner, class_name: 'User'
+  has_many :votes, dependent: :destroy
+  has_many :users, through: :votes
+  has_many :comments, dependent: :destroy
 end
