0001890acf75b0f2496d80f2c5d04c16677ec1fe
Marvin Carrasco <marccenvivo@gmail.com>
2020-06-04 13:26:14 -0500

    add associations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,9 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  has_many :comments
+  has_many :commentators through: :comments, source: :user
+
+  has_many :likes
+  has_many :likers through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :comments
+  has_many :commented_tweets, through: :comments, source: :tweet
+
+  has_many :likes
+  has_many :liked_tweets, through: :likes, source: :tweet
 end
