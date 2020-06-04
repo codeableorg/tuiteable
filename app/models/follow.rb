a6176adf7de81e63759016fda4b5c3425b6c295e
Jorge Gonzalez <jorg3.594@gmail.com>
2020-06-03 20:21:43 -0500

    countercache

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,17 +1,4 @@
 class Follow < ApplicationRecord
-  belongs_to :following, class_name: "User"
-  belongs_to :follower, class_name: "User"
-
-  after_create do
-    follower.followings_count += 1
-    following.followers_count += 1
-    follower.save
-    following.save
-  end
-  after_destroy do
-   follower.followings_count -= 1
-   following.followers_count -= 1
-   follower.save
-   following.save
-  end
+  belongs_to :following, class_name: "User", counter_cache: :followers_count
+  belongs_to :follower, class_name: "User", counter_cache: :followings_count
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :comented_tweets, class_name: "Tweet", through: :comments
   has_many :followers, class_name: "Follow", foreign_key: "following_id"
-  has_many :followings, class_name: "Follow", foreign_key: "followers_id"
+  has_many :followings, class_name: "Follow", foreign_key: "follower_id"
   has_many :user_followers, through: :followers, source: :follower
   has_many :user_followings, through: :followings, source: :following
 end
