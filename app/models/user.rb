11c5bd89195164352f8b7ad329cfa3752ea5af33
Rai <raikcuerda19@gmail.com>
2020-06-04 13:07:07 -0500

    visitor can sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook]
+         :omniauthable, omniauth_providers: %i[facebook github]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -13,4 +13,21 @@ class User < ApplicationRecord
       user.username = auth.info.name.downcase.gsub(/\s/,"") + rand(1..10000).to_s
     end
   end
+
+  def self.from_omniauth_git(auth)
+    p auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      email = auth.info.email
+      if email.nil?
+        user.email = "email@noemail.com"
+      else
+        user.email = email
+      end
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.username = auth.info.nickname
+    end
+  end
+
 end
+
