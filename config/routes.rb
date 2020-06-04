14ff14e289d113a194b8a2eb4a9d4dc9965702e6
sebas_z <zanabria.s@pucp.pe>
2020-06-03 22:33:03 -0500

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,22 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  resources :tuits, only: [:index, :show]
+  namespace :api do
+    resources :tuits, only: [:index, :show, :create, :update, :destroy]
+    resources :users, only: [] do
+      resources :tuits, only: [:index, :show, :create, :update, :destroy]
+      get '/followers', to: 'follows#show'
+      get '/followings', to: 'follows#show'
+      get '/follow', to: 'follows#create'
+      get '/follow', to: 'follows#destroy'
+    end
+    resources :tuits, only: [] do
+      resources :comments, only: [:index, :show, :create, :update, :destroy]
+    end
+    resources :tuits, only: [] do
+      resources :likes, only: [:index, :create, :destroy]
+    end
+  end
+  
 end
