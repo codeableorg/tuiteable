72996b3fa21fde86109bee6b5d2fc82d2de32381
waterquarks <waterquarks@protonmail.com>
2020-06-05 11:52:22 -0500

    add profile pics to users

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,10 @@
 class Tweet < ApplicationRecord
+  # Relations
   belongs_to :user
   belongs_to :parent, class_name: 'Tweet', optional: true, counter_cache: :responses_count
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
   has_many :responses, class_name: 'Tweet', foreign_key: 'parent_id', dependent: :destroy
+  # Validations
   validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,9 @@ class User < ApplicationRecord
   has_many :tweets,  through: :likes
   # Attachments
   has_one_attached :avatar
+  # Misc
+  before_create do
+    self.name = self.tag
+    self.avatar.attach(io: File.open('app/assets/images/profile-picture-placeholder.png'), filename: 'pic.png')
+  end
 end
