bc0a485e478abc0adbedc786607feeb89edff76d
Albert <acastemoreno@gmail.com>
2020-06-05 11:49:44 -0500

    api spec: group current test by action method

--- spec/controllers/api/tweets_controller_spec.rb
+++ spec/controllers/api/tweets_controller_spec.rb
@@ -2,30 +2,40 @@ require 'rails_helper'
 require 'bcrypt'
 
 describe Api::TweetsController do
-  before :each do
-    @user = User.create(username: "test", name: "test", "email" => "test@gmail.com", password: "123456")
-    @tweet = Tweet.create(owner_id: @user.id, body: "tweet body")
+  describe 'GET to index' do
+    before :each do
+      @user = User.create(username: "test", name: "test", "email" => "test@gmail.com", password: "123456")
+      @tweet = Tweet.create(owner_id: @user.id, body: "tweet body")
+    end
+
+    it 'returns http status ok' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'render json with all tweets' do
+      get :index
+      tweets = JSON.parse(response.body)
+      expect(tweets.size).to eq 1
+    end
+
   end
 
-  it 'returns http status ok' do
-    get :index
-    expect(response).to have_http_status(:ok)
-  end
+  describe 'GET to show' do
+    before :each do
+      @user = User.create(username: "test", name: "test", "email" => "test@gmail.com", password: "123456")
+      @tweet = Tweet.create(owner_id: @user.id, body: "tweet body")
+    end
+    
+    it 'returns http status ok' do
+      get :show, params: {id: @tweet.id}
+      expect(response).to have_http_status(:ok)
+    end
 
-  it 'render json with all tweets' do
-    get :index
-    tweets = JSON.parse(response.body)
-    expect(tweets.size).to eq 1
-  end
-
-  it 'returns http status ok' do
-    get :show, params: {id: @tweet.id}
-    expect(response).to have_http_status(:ok)
-  end
-
-  it 'render json with all tweets' do
-    get :show, params: {id: @tweet.id}
-    tweet = JSON.parse(response.body)
-    expect(tweet["body"]).to eq "tweet body"
+    it 'render json with all tweets' do
+      get :show, params: {id: @tweet.id}
+      tweet = JSON.parse(response.body)
+      expect(tweet["body"]).to eq "tweet body"
+    end
   end
 end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 describe Api::UsersController do
 
-  it 'returns http status ok' do
-    post :create, params: {user: {username: "test", name: "test", "email" => "test@gmail.com", password: "123456"}}
-    expect(response).to have_http_status(:created)
-  end
+  describe 'POST to create' do
+    it 'returns http status ok' do
+      post :create, params: {user: {username: "test", name: "test", "email" => "test@gmail.com", password: "123456"}}
+      expect(response).to have_http_status(:created)
+    end
 
-  it 'render json with all tweets' do
-    post :create, params: {user: {username: "test", name: "test", "email" => "test@gmail.com", password: "123456"}}
-    user = JSON.parse(response.body)
-    expect(user["username"]).to eq "test"
-    expect(user["name"]).to eq "test"
-    expect(user["email"]).to eq "test@gmail.com"
+    it 'render json with all tweets' do
+      post :create, params: {user: {username: "test", name: "test", "email" => "test@gmail.com", password: "123456"}}
+      user = JSON.parse(response.body)
+      expect(user["username"]).to eq "test"
+      expect(user["name"]).to eq "test"
+      expect(user["email"]).to eq "test@gmail.com"
+    end
   end
 end
