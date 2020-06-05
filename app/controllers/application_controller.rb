0353bc1f93590a89ba3a01bfa5e56bda828a499d
FrankFlores <fraco.1918@gmail.com>
2020-06-05 11:29:21 -0500

    added profile view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   # # include Pundit
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :email, :location, :bio])
+  end
   # def index
   # end
 
