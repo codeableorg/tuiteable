24baf30b80bbae8521f920606bb93daee6713c70
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-06-04 21:24:26 -0500

    user can react to tuit from any page

--- app/controllers/tuits/likes_controller.rb
+++ app/controllers/tuits/likes_controller.rb
@@ -2,14 +2,25 @@ class Tuits::LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    tuit = Tuit.find(params[:tuit_id])
-    like = Like.find_by(user: current_user, tuit: tuit)
-    if like
-      redirect_to tuit_path(tuit), alert: 'You already have liked this tuit.'
-    elsif tuit.likes.new(user: current_user)&.save
-      redirect_to tuit_path(tuit)
+    p tuit = Tuit.find(params[:tuit_id])
+    p like = Like.find_by(user: current_user, tuit: tuit)
+    p params
+    p path = params[:redirect] ? params[:redirect] : tuit_path(tuit)
+
+    if like.nil?
+      like = tuit.likes.new(user: current_user)
+      if like.save
+        redirect_to path
+      else
+        redirect_to path, alert: 'We could not add your like.'
+      end
     else
-      redirect_to tuit_path(tuit), alert: 'We could not add your like.'
+      redirect_to path, alert: 'You already like this tuit.'
     end
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:referrer)
   end
 end
