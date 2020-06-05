f7704eccc7a2932144dad639c4b5943ccd54b3ca
Jorge Gonzalez <jorg3.594@gmail.com>
2020-06-05 13:23:17 -0500

    login-signup-signout

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
     resources :tweets, only: [:index]
     resources :favorites, only: [:index]
   end
+  namespace :api do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+        delete "sign_out", to: "destroy_sessions#destroy"
+      end
+      resources :tweets, only: [:index]
+  end
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
