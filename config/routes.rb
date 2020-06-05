6643563b1a55a9f99e88a97d1a8b32e6ef04ea00
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-06-05 02:07:57 -0500

    added layout logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "tuits#index"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/profile', to: 'users#show'
   resources :tuits do
     resources :comments
   end
