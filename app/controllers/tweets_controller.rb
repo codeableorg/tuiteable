159070e7d0f1cb8f74c9216e9aa13b4dfc07aa60
MARIELLA <mariella.ugarte@utec.edu.pe>
2020-06-04 23:26:47 -0500

    react

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,29 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, only: :destroy
+  before_action :authenticate_user!, only: [:destroy, :like]
+
+  def index
+    @tweets = Tweet.all
+  end
 
   def destroy
     @tweet = Tweet.find(params[:id])
     authorize @tweet
     @tweet.destroy
     redirect_to tweets_path()
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def like
+    @tweet = Tweet.find(params[:id])
+    p @tweet.likers.count
+    if current_user.liked_tweets.where(:id => params[:id]).blank?
+      @tweet.likers << current_user
+    else
+      @tweet.likers.delete(current_user)
+    end
+    redirect_to @tweet
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,25 +1,15 @@
 class Tweet < ApplicationRecord
+  attr_accessor :likes
+
   ## Association
   belongs_to :owner, class_name: 'User'
-  has_many :comments, dependent: :destroy
+  has_many :comments, -> { order created_at: :desc }, dependent: :destroy
   has_many :commentators, through: :comments, source: :user
   has_and_belongs_to_many :likers,
     join_table: "likes",
-    class_name: "User",
-    after_add: :add_like,
-    after_remove: :remove_like
+    class_name: "User"
 
   before_destroy do |tweet| 
     tweet.likers.destroy(tweet.likers) ## Destroy likers and allow callbacks to execute
   end
-
-  ##Callback
-  def add_like(liker)
-    puts "Holi desde tweet"
-  end
-
-  def remove_like(liker)
-    puts "Holi desde tweet"
-  end
-  
 end
