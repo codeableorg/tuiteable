25b60b2fac20b609bab2cf4840f48af54a287935
Rai <raikcuerda19@gmail.com>
2020-06-04 20:02:00 -0500

    user can see a profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'porfile' ,to: 'users#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'tweets#index'
 
