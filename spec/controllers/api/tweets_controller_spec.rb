2fbcc9ddd632fc501e2532bfa04cb46db358ddaa
Albert <acastemoreno@gmail.com>
2020-06-05 11:53:35 -0500

    api spec: create tweet

--- spec/controllers/api/tweets_controller_spec.rb
+++ spec/controllers/api/tweets_controller_spec.rb
@@ -26,7 +26,7 @@ describe Api::TweetsController do
       @user = User.create(username: "test", name: "test", "email" => "test@gmail.com", password: "123456")
       @tweet = Tweet.create(owner_id: @user.id, body: "tweet body")
     end
-    
+
     it 'returns http status ok' do
       get :show, params: {id: @tweet.id}
       expect(response).to have_http_status(:ok)
@@ -36,6 +36,25 @@ describe Api::TweetsController do
       get :show, params: {id: @tweet.id}
       tweet = JSON.parse(response.body)
       expect(tweet["body"]).to eq "tweet body"
+    end
+  end
+
+  describe 'POST to create' do
+    before :each do
+      @user = User.create(username: "test", name: "test", "email" => "test@gmail.com", password: "123456")
+      @request.headers['X-User-Email'] = "test@gmail.com"
+      @request.headers['X-User-Token'] = @user.authentication_token
+    end
+
+    it 'returns http status created' do
+      post :create, params: {tweet: {body: "holi"}}
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'render json with all tweets' do
+      post :create, params: {tweet: {body: "holi"}}
+      tweet = JSON.parse(response.body)
+      expect(tweet["body"]).to eq "holi"
     end
   end
 end
