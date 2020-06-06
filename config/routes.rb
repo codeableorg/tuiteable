1109aef89b2a745c4b8563de87aa280cdc8d0255
Rai <raikcuerda19@gmail.com>
2020-06-05 22:37:19 -0500

    public Api

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,25 @@ Rails.application.routes.draw do
   resources :tweets, only: [:index, :show] do
     resources :comments, only: [:create]
   end
+
+  namespace :api do
+
+    post 'sessions/sign_in', to: 'sessions#create'
+    
+    resources :tweets, only: [:index, :show, :create, :destroy]
+
+    resources :users , only: [:index] do
+      resources :tweets, only: [:index, :show]
+    end
+
+    resources :tweets , only: [:index, :show] do
+      resources :comments, only: [:index, :show, :create,:destroy]
+    end
+
+    resources :tweets , only: [:index] do
+      resources :likes, only: [:index, :create,:destroy]
+    end
+  
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
