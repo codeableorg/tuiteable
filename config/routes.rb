29518e422cea407df1edeb1cc5ea9d1b38ea633e
Freddy Munive <munivef@yahoo.com>
2020-06-05 23:26:35 -0500

    adding button to follow but it doesn't work weel, check controller create follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'user/show'
   get 'user/create'
+  
+  # resources :users, only: [:show, :new, :create] do
+  resources :follows, only: [:create, :destroy]
+  # end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "home#index"
   resources :tuits, only: [:index, :show, :new, :create, :destroy]
-  resources :follows, only: [:create, :destroy]
   resources :tuits do
     resources :likes, module: :tuits, only: [:create, :destroy]
     resources :comments, module: :tuits, only: [:create]
