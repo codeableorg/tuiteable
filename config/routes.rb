8f678a5e58c7911705a8676675b2b24a87b06ae3
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-06-05 23:56:51 -0500

    final?

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get 'users/show'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   resources :users, only: :show
   get 'home/index'
   resources :tuits
