cb063fab11e8e4e15377d3dfd9305084a3e928bc
alexandell <alexander.chavarria@utec.edu.pe>
2020-06-05 20:53:03 -0500

    test TDD

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,14 @@ Rails.application.routes.draw do
   root to: 'pages#root'
   get '/:tag', to: 'users#show'
   get '/:tag/:id', to: 'tweets#show'
+  ## API
+  namespace :api do
+    resources :tweets
+  end
+  namespace :api do
+    resources :users
+  end
+  namespace :api do
+  resources :likes
+  end
 end
