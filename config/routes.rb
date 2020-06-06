5c5919417cd1df5ae197045f4fbfd56e9d6d7729
Josue Narro <josue.narro.a@gmail.com>
2020-06-05 21:31:24 -0500

    User can add a comment to a tuit

--- config/routes.rb
+++ config/routes.rb
@@ -6,23 +6,25 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: :show
   get 'home/index'
-  resources :tuits
+  resources :tuits do
+    resources :comments, only: :create
+  end
   root to: 'tuits#index'
   get '/my_profile', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # API
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: { format: :json } do
     resources :tuits, only: [:create]
 
     devise_scope :user do
-      post "users", to: "registration#create"
+      post 'users', to: 'registration#create'
     end
 
-    namespace :session do 
+    namespace :session do
       devise_scope :user do
-        post "sign_in", to: "session#create"
-        delete "logout", to: "session#logout"
+        post 'sign_in', to: 'session#create'
+        delete 'logout', to: 'session#logout'
       end
     end
   end
